0236fa1f900543aa05a4df3c42f5981848ee1982
pablopinto85 <pablo.pinto1985@gmail.com>
2023-09-03 01:50:42 -0300

    v1.1

--- components/productmanager.js
+++ components/productmanager.js
@@ -135,7 +135,7 @@ class ProductManager {
   const updateResult = await productManager.updateProduct(1, { price: 180, stock: 18 });
   console.log("Producto actualizado:", updateResult ? "Éxito" : "Fallo");
 
-  const deleteResult = await productManager.deleteProduct(9);
+  const deleteResult = await productManager.deleteProduct(3);
   console.log("Producto eliminado:", deleteResult ? "Éxito" : "Fallo");
 })();
 
